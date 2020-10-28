0fa9f3ab10a24e44a6ab2025036f1614f53f88f0
Dolbow <Mike.Dolbow@state.mn.us>
2020-10-28 15:56:33 -0500

    schoolsdistricts SQL and readme update

--- schoolsdistricts.sql
+++ schoolsdistricts.sql
@@ -1,145 +1,8 @@
-# SQLPond
-For dipping your toes into the SQL Pond!
+/*Build and query a database of some school districts and schools in Minnesota
+This file is the space where we build and test this concept with SQL hints in an IDE. When we're finished,
+we dump the SQL into the readme for nicer formatting.
+ */
 
-This repository is filled with examples of SQL code that can be used to build and explore data, focused on commonly used data in the State of Minnesota.
-
-## SQL Playgrounds
-Want to test out this code? Try using one of the following:
-1. http://sqlfiddle.com/ : Has worked some tries, not so much others. Note you have to build the full schema (including inserts) before doing queries. When successful, links will be posted below.
-2. https://www.w3schools.com/sql/trysql.asp?filename=trysql_create_table : build a db in your browser (SQLite back end). Kind of tedious (inserts only one at a time for example), but works and has a quick Restore function
-
-### SQL Fiddle Links
-1. [A unique list of cities](http://sqlfiddle.com/#!18/da6d9/7/0) with manipulated names based on various conditions
-
-### Formatted SQL Exercises
-In the sections below, you'll find SQL you can copy and paste into a website like sqlfiddle.com (be sure to switch to MS SQL Server style!). Build the schema first, then query it.
-
-#### Build and Query County and City database
-SQL to build and query a database of some common cities and counties in Minnesota.
-##### Build Schema
-```SQL
-/******BEGIN BUILD SCHEMA *********/
---create the county table
-CREATE TABLE COUNTY(
-	COUNTYFIPS smallint NOT NULL,
-	CTY_NAME varchar(20) NULL
-)
-
---create the CTU table
-CREATE TABLE CTU(
-    GNISTXT varchar(8) NOT NULL,
-	FEATNAME varchar(40) NULL,
-    CTUTYPE varchar(24) NULL,
-	CTYNUM varchar(3) NULL
-)
-
---Insert values into COUNTY table (one at a time at W3schools)
-INSERT INTO COUNTY (COUNTYFIPS,CTY_NAME) VALUES (27003,'Anoka');
-INSERT INTO COUNTY (COUNTYFIPS,CTY_NAME) VALUES (27053,'Hennepin');
-INSERT INTO COUNTY (COUNTYFIPS,CTY_NAME) VALUES (27145,'Stearns');
-INSERT INTO COUNTY (COUNTYFIPS,CTY_NAME) VALUES (27123,'Ramsey');
-INSERT INTO COUNTY (COUNTYFIPS,CTY_NAME) VALUES (27021,'Cass');
-INSERT INTO COUNTY (COUNTYFIPS,CTY_NAME) VALUES (27147,'Steele');
-
---Insert values into the CITY table (one at a time at W3schools)
-INSERT INTO CTU (GNISTXT,FEATNAME,CTUTYPE,CTYNUM) VALUES ('02396471','Saint Anthony','City','053');
-INSERT INTO CTU (GNISTXT,FEATNAME,CTUTYPE,CTYNUM) VALUES ('02396471','Saint Anthony','City','123');
-INSERT INTO CTU (GNISTXT,FEATNAME,CTUTYPE,CTYNUM) VALUES ('02396472','Saint Anthony','City','145');
-INSERT INTO CTU (GNISTXT,FEATNAME,CTUTYPE,CTYNUM) VALUES ('00663943','Deerfield','Township','021');
-INSERT INTO CTU (GNISTXT,FEATNAME,CTUTYPE,CTYNUM) VALUES ('00663944','Deerfield','Township','147');
-INSERT INTO CTU (GNISTXT,FEATNAME,CTUTYPE,CTYNUM) VALUES ('00663487','Avon','Township','145');
-INSERT INTO CTU (GNISTXT,FEATNAME,CTUTYPE,CTYNUM) VALUES ('02394043','Avon','City','145');
-INSERT INTO CTU (GNISTXT,FEATNAME,CTUTYPE,CTYNUM) VALUES ('02394183','Blaine','City','003');
-INSERT INTO CTU (GNISTXT,FEATNAME,CTUTYPE,CTYNUM) VALUES ('02394183','Blaine','City','123');
-INSERT INTO CTU (GNISTXT,FEATNAME,CTUTYPE,CTYNUM) VALUES ('02395261','North Saint Paul','City','123');
-INSERT INTO CTU (GNISTXT,FEATNAME,CTUTYPE,CTYNUM) VALUES ('02396511','Saint Paul','City','123');
-/******END BUILD SCHEMA *********/
-```
-##### Query the city and county  tables
-```SQL
-
-/******INDIVIDUAL QUERIES *********/
---Query the COUNTY table with some basic manipulations
-SELECT 
-COUNTYFIPS AS 'CODE'
-,CTY_NAME AS 'NAME'
-FROM [COUNTY]
-
---Query the COUNTY table and mod the name column (using || for SQL Lite concatenation)
-SELECT 
-COUNTYFIPS AS 'CODE'
---,CTY_NAME || ' County' AS 'NAME' --uses SQL Lite concatenation method, for w3schools
-,CTY_NAME + ' County' AS 'NAME' --SQL Server syntax, for sqlfiddle.com
-FROM [COUNTY]
-
---Query the CTU table with some manipulations
-SELECT
-'27' + CTYNUM + GNISTXT AS GNISID
-,CASE WHEN CTUTYPE = 'Township' THEN FEATNAME + ' Twp.'
-	WHEN GNISTXT = '02396471' THEN 'St. Anthony Village'
-	ELSE FEATNAME
-END AS 'NAME'
-FROM [CTU]
-
---Join the two tables to see how all columns look together
-SELECT * FROM [CTU]
-LEFT JOIN [COUNTY] ON [CTU].[CTYNUM]+27000 = [COUNTY].[COUNTYFIPS]
-
---Now change some columns to get a customized view
-SELECT
-CTU.FEATNAME
-,CTU.CTUTYPE AS TYPE
-,COUNTY.COUNTYFIPS
-,COUNTY.CTY_NAME + ' County' AS COUNTY
-FROM [CTU]
-LEFT JOIN [COUNTY] ON [CTU].[CTYNUM]+27000 = [COUNTY].[COUNTYFIPS]
-
---Let's pull that all together
-SELECT 
-'27' + CTU.CTYNUM + CTU.GNISTXT AS GNISID
-,CASE WHEN CTU.CTUTYPE = 'Township' THEN CTU.FEATNAME + ' Twp.'
-	WHEN CTU.GNISTXT = '02396471' THEN 'St. Anthony Village'
-	ELSE CTU.FEATNAME
-END AS 'NAME'
-,CTU.CTUTYPE AS TYPE
-,COUNTY.COUNTYFIPS
-,COUNTY.CTY_NAME AS COUNTY
-FROM [CTU]
-LEFT JOIN [COUNTY] ON [CTU].[CTYNUM]+27000 = [COUNTY].[COUNTYFIPS]
-
---What if we want to know more about some duplicate names?
-SELECT
-FEATNAME
-,COUNT(FEATNAME) AS COUNT
-FROM [CTU]
-GROUP BY FEATNAME
-HAVING COUNT(FEATNAME) > 1
-
---so if we wanted to qualify township names with county names:
-SELECT 
-'27' + CTU.CTYNUM + CTU.GNISTXT AS GNISID
-,CASE WHEN DUPES.FEATNAME IS NOT NULL AND CTU.CTUTYPE = 'Township' THEN CTU.FEATNAME + ' Twp.' + ' ('+COUNTY.CTY_NAME+')'
-	WHEN DUPES.FEATNAME IS  NULL AND CTU.CTUTYPE = 'Township' THEN CTU.FEATNAME + ' Twp.'
-	WHEN CTU.GNISTXT = '02396471' THEN 'St. Anthony Village'
-	ELSE CTU.FEATNAME
-END AS 'NAME'
-,CTU.CTUTYPE AS TYPE
-,COUNTY.COUNTYFIPS
-FROM [CTU]
-LEFT JOIN [COUNTY] ON [CTU].[CTYNUM]+27000 = [COUNTY].[COUNTYFIPS]
-LEFT JOIN (SELECT
-	FEATNAME --note we only want the feature name, don't need the count
-	FROM [CTU]
-	WHERE [CTU].CTUTYPE = 'Township' --note we only care about townships
-	GROUP BY FEATNAME
-	HAVING COUNT(FEATNAME) > 1) AS DUPES ON CTU.FEATNAME = DUPES.FEATNAME
-```
-
-#### Build and Query Schools and School District database
-SQL to build and query a database of some schools and districts in Minnesota.
-
-##### Build Schema
-```SQL
 /******BEGIN BUILD SCHEMA *********/
 --Create the school district table
 CREATE TABLE SCHOOL_DISTRICT(
@@ -193,10 +56,7 @@ INSERT INTO SCHOOLS (ORGID,SCHNAME,ADDRESS,CITY,ZIP) VALUES (12909031000,'Evelet
 INSERT INTO SCHOOLS (ORGID,SCHNAME,ADDRESS,CITY,ZIP) VALUES (12909030000,'Roosevelt Elem','411 5th Ave S','Virginia','55792')
 
 /******END BUILD SCHEMA *********/
-```
 
-##### Query the city and county  tables
-```SQL
 /******INDIVIDUAL QUERIES *********/
 --Get records from the school district table
 SELECT * FROM SCHOOL_DISTRICT
@@ -280,4 +140,3 @@ SELECT
     ,CONCAT(s.ADDRESS,', ',s.CITY,', MN ',s.ZIP) as Address
 FROM SCHOOLS s
 LEFT JOIN SCHOOL_DISTRICT d ON d.ORGID = FLOOR(s.ORGID/1000000)*1000000
-```
